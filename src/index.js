9017cd9394bb7053601305fc38b3f215cbc183b6
Guy Arieli <guyarieli1997@gmail.com>
2021-04-17 21:48:16 +0300

    added service workers

--- src/index.js
+++ src/index.js
@@ -3,5 +3,8 @@ import ReactDOM from "react-dom";
 import App from "./components/App/App";
 import "./styles/_setting.scss";
 import "./styles/_base.scss";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 
 ReactDOM.render(<App />, document.getElementById("root"));
+
+serviceWorkerRegistration.register();
